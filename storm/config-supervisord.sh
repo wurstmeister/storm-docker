ec56e5041cb0f9117a48c217aac37b820557160a
wurstmeister <wurstmeister@users.noreply.github.com>
2014-07-13 20:45:11 +0100

    Update config-supervisord.sh

--- storm/config-supervisord.sh
+++ storm/config-supervisord.sh
@@ -2,4 +2,4 @@ echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo user=storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
